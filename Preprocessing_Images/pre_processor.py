18dbc4bdd93508d02dfab77b66cd68a8e7c11281
Lalbadshah <pbhatna3@jhu.edu>
2017-11-17 12:11:27 -0500

    Downscaling now actually works and is fast AF

--- Preprocessing_Images/pre_processor.py
+++ Preprocessing_Images/pre_processor.py
@@ -1,15 +1,15 @@
 import cv2
 import os
+import numpy as np
 path = '/home/butterchicken/Documents/MLSP_Project_Code/mlsp_project/Preprocessing_Images/'
 def load_images_from_folder(folder = path+'original_data/'):
     directory = path + 'Downscaled_Images/'
+    dimg = np.zeros((500,500,3))
     if not os.path.exists(directory):
         os.makedirs(directory)
     for filename in os.listdir(folder):
         img = cv2.imread(os.path.join(folder,filename))
-        fx = img.shape[0] / 500
-        fy = img.shape[1] / 500
-        dimg = cv2.resize(img, None, 0, fx, fy)
+        dimg = cv2.resize(img,(500,500))
         cv2.imwrite(directory + filename, dimg)
 
 
